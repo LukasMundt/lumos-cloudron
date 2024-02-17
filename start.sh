155feb276291cf9b3aa983db1deee335466a8471
Lukas Mundt <lukas@lukas-mundt.de>
2024-02-17 23:18:03 +0100

    init

---
+++ start.sh
@@ -0,0 +1,100 @@
+#!/bin/bash
+
+set -eu
+
+mkdir -p /run/apache2 /run/lumos/sessions /run/lumos/logs /app/data/apache
+
+readonly ARTISAN="sudo -E -u www-data php /app/code/artisan"
+
+if [[ ! -f /app/data/.cr ]]; then
+    echo "=> First run"
+    mkdir -p /app/data/storage
+    cp -R /app/code/storage.template/* /app/data/storage
+    cp /app/code/env.production.template /app/data/env
+
+    chown -R www-data:www-data /run/lumos /app/data
+
+    echo "=> Generating app key"
+    $ARTISAN key:generate --force --no-interaction
+
+    echo "=> Run migrations and seed database"
+    $ARTISAN lumos:install --force
+
+    # echo "=> Create the access tokens required for the API"
+    # $ARTISAN passport:keys --force
+    # $ARTISAN passport:client --personal --no-interaction
+
+    touch /app/data/.cr
+else
+    echo "=> Existing installation. Running migration script"
+    chown -R www-data:www-data /run/lumos /app/data
+    $ARTISAN lumos:update --force
+fi
+
+
+# generate files if neither index.* or .htaccess
+# if [[ -z "$(ls -A /app/data/public)" ]]; then
+#     echo "==> Generate files on first run" # possibly not first run if user deleted index.*
+#     cp /app/code/index.php /app/data/public/index.php
+#     echo -e "#!/bin/bash\n\n# Place custom startup commands here" > /app/data/run.sh
+#     touch /app/data/public/.htaccess
+# else
+#     echo "==> Do not override existing index file"
+# fi
+
+
+if [[ ! -f /app/data/php.ini ]]; then
+    echo -e "; Add custom PHP configuration in this file\n; Settings here are merged with the package's built-in php.ini; Restart the app for any changes to take effect\n\n" > /app/data/php.ini
+fi
+
+[[ ! -f /app/data/apache/mpm_prefork.conf ]] && cp /app/code/apache/mpm_prefork.conf /app/data/apache/mpm_prefork.conf
+[[ ! -f /app/data/apache/app.conf ]] && cp /app/code/apache/app.conf /app/data/apache/app.conf
+[[ ! -f /app/data/PHP_VERSION ]] && echo -e "; Set the desired PHP version in this file\n; Restart app for changes to take effect\nPHP_VERSION=8.1" > /app/data/PHP_VERSION
+
+readonly php_version=$(sed -ne 's/^PHP_VERSION=\(.*\)$/\1/p' /app/data/PHP_VERSION)
+echo "==> PHP version set to ${php_version}"
+ln -sf /etc/apache2/mods-available/php${php_version}.conf /run/apache2/php.conf
+ln -sf /etc/apache2/mods-available/php${php_version}.load /run/apache2/php.load
+
+# source it so that env vars are persisted
+echo "==> Source custom startup script"
+[[ -f /app/data/run.sh ]] && source /app/data/run.sh
+
+[[ -f /app/data/crontab ]] && echo -e "\n\033[0;31mWARNING: crontab support has been removed. Please move cron tasks to the cron section of the app. See https://docs.cloudron.io/apps/#cron for more information.\033[0m\n"
+
+# phpMyAdmin auth file
+if [[ ! -f /app/data/.phpmyadminauth ]]; then
+    echo "==> Generating phpMyAdmin authentication file"
+    PASSWORD=`pwgen -1 16`
+    htpasswd -cb /app/data/.phpmyadminauth admin "${PASSWORD}"
+    sed -e "s,PASSWORD,${PASSWORD}," /app/code/phpmyadmin_login.template > /app/data/phpmyadmin_login.txt
+fi
+
+echo "==> Creating credentials.txt"
+sed -e "s,\bMYSQL_HOST\b,${CLOUDRON_MYSQL_HOST}," \
+    -e "s,\bMYSQL_PORT\b,${CLOUDRON_MYSQL_PORT}," \
+    -e "s,\bMYSQL_USERNAME\b,${CLOUDRON_MYSQL_USERNAME}," \
+    -e "s,\bMYSQL_PASSWORD\b,${CLOUDRON_MYSQL_PASSWORD}," \
+    -e "s,\bMYSQL_DATABASE\b,${CLOUDRON_MYSQL_DATABASE}," \
+    -e "s,\bMYSQL_URL\b,${CLOUDRON_MYSQL_URL}," \
+    -e "s,\bMAIL_SMTP_SERVER\b,${CLOUDRON_MAIL_SMTP_SERVER}," \
+    -e "s,\bMAIL_SMTP_PORT\b,${CLOUDRON_MAIL_SMTP_PORT}," \
+    -e "s,\bMAIL_SMTPS_PORT\b,${CLOUDRON_MAIL_SMTPS_PORT}," \
+    -e "s,\bMAIL_SMTP_USERNAME\b,${CLOUDRON_MAIL_SMTP_USERNAME}," \
+    -e "s,\bMAIL_SMTP_PASSWORD\b,${CLOUDRON_MAIL_SMTP_PASSWORD}," \
+    -e "s,\bMAIL_FROM\b,${CLOUDRON_MAIL_FROM}," \
+    -e "s,\bMAIL_DOMAIN\b,${CLOUDRON_MAIL_DOMAIN}," \
+    -e "s,\bREDIS_HOST\b,${CLOUDRON_REDIS_HOST:-NA}," \
+    -e "s,\bREDIS_PORT\b,${CLOUDRON_REDIS_PORT:-NA}," \
+    -e "s,\bREDIS_PASSWORD\b,${CLOUDRON_REDIS_PASSWORD:-NA}," \
+    -e "s,\bREDIS_URL\b,${CLOUDRON_REDIS_URL:-NA}," \
+    /app/code/credentials.template > /app/data/credentials.txt
+
+chown -R www-data:www-data /app/data /run/apache2 /run/lumos /tmp
+
+echo "=> Starting Apache"
+APACHE_CONFDIR="" source /etc/apache2/envvars
+rm -f "${APACHE_PID_FILE}"
+exec /usr/sbin/apache2 -DFOREGROUND
+
+
